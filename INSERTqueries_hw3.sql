bd66dc81589de5fd1881f7b90c1ca921dc6e6405
norrljus <thatfoxdoe@gmail.com>
2024-10-25 12:27:14 +0500

    hw3

---
+++ INSERTqueries_hw3.sql
@@ -0,0 +1,67 @@
+INSERT INTO genres(name) VALUES ('Rock');
+INSERT INTO genres(name) VALUES ('Alternative');
+INSERT INTO genres(name) VALUES ('Experimental');
+INSERT INTO genres(name) VALUES ('Indie');
+
+
+INSERT INTO artists(name) VALUES ('The Strokes');
+INSERT INTO artists(name) VALUES ('The Shins');
+INSERT INTO artists(name) VALUES ('Stomach Book');
+INSERT INTO artists(name) VALUES ('Car Seat Headrest');
+INSERT INTO artists(name) VALUES ('Mitski');
+
+
+INSERT INTO albums(name, year) VALUES ('The New Abnormal', 2020);
+INSERT INTO albums(name, year) VALUES ('Chutes Too Narrow', 2003);
+INSERT INTO albums(name, year) VALUES ('Stomach Book', 2021);
+INSERT INTO albums(name, year) VALUES ('Twin Fantasy', 2011);
+INSERT INTO albums(name, year) VALUES ('Be the Cowboy', 2018);
+
+
+INSERT INTO tracks(name, length, album) VALUES ('The Adults Are Talking', 5.10, 1);
+INSERT INTO tracks(name, length, album) VALUES ('Selfless', 3.43, 1);
+INSERT INTO tracks(name, length, album) VALUES ('Brooklyn Bridge To Chorus', 3.56, 1);
+INSERT INTO tracks(name, length, album) VALUES ('Kissing The Lipless', 3.19, 2);
+INSERT INTO tracks(name, length, album) VALUES ('Mine is Not a High Horse', 3.20, 2);
+INSERT INTO tracks(name, length, album) VALUES ('Animals', 3.15, 3);
+INSERT INTO tracks(name, length, album) VALUES ('Our Story', 5.11, 3);
+INSERT INTO tracks(name, length, album) VALUES ('Sober to Death', 5.05, 4);
+INSERT INTO tracks(name, length, album) VALUES ('Bodys', 6.47, 4);
+INSERT INTO tracks(name, length, album) VALUES ('Me and My Husband', 2.18, 5);
+INSERT INTO tracks(name, length, album) VALUES ('Nobody', 3.14, 5);
+INSERT INTO tracks(name, length, album) VALUES ('Washing Machine Heart', 2.09, 5);
+
+
+INSERT INTO collections(name, year) VALUES ('Alternative Rock', 2021);
+INSERT INTO collections(name, year) VALUES ('Experimental Collection', 2022);
+INSERT INTO collections(name, year) VALUES ('Indie Music', 2018);
+INSERT INTO collections(name, year) VALUES ('The Shins Best Tracks', 2010);
+
+
+INSERT INTO artistsgenres(genreid, artistid) VALUES (1, 1);
+INSERT INTO artistsgenres(genreid, artistid) VALUES (1, 2);
+INSERT INTO artistsgenres(genreid, artistid) VALUES (2, 2);
+INSERT INTO artistsgenres(genreid, artistid) VALUES (2, 4);
+INSERT INTO artistsgenres(genreid, artistid) VALUES (3, 3);
+INSERT INTO artistsgenres(genreid, artistid) VALUES (4, 4);
+INSERT INTO artistsgenres(genreid, artistid) VALUES (4, 5);
+
+
+INSERT INTO albumartists(artistid, albumid) VALUES (1, 1);
+INSERT INTO albumartists(artistid, albumid) VALUES (2, 2);
+INSERT INTO albumartists(artistid, albumid) VALUES (3, 3);
+INSERT INTO albumartists(artistid, albumid) VALUES (4, 4);
+INSERT INTO albumartists(artistid, albumid) VALUES (5, 5);
+
+
+INSERT INTO collectiontracks(trackid, collectionid) VALUES (4, 1);
+INSERT INTO collectiontracks(trackid, collectionid) VALUES (5, 1);
+INSERT INTO collectiontracks(trackid, collectionid) VALUES (8, 1);
+INSERT INTO collectiontracks(trackid, collectionid) VALUES (9, 1);
+INSERT INTO collectiontracks(trackid, collectionid) VALUES (6, 2);
+INSERT INTO collectiontracks(trackid, collectionid) VALUES (7, 2);
+INSERT INTO collectiontracks(trackid, collectionid) VALUES (10, 3);
+INSERT INTO collectiontracks(trackid, collectionid) VALUES (11, 3);
+INSERT INTO collectiontracks(trackid, collectionid) VALUES (9, 3);
+INSERT INTO collectiontracks(trackid, collectionid) VALUES (4, 4);
+INSERT INTO collectiontracks(trackid, collectionid) VALUES (5, 4);
---
+++ SELECTqueries_hw3.sql
@@ -0,0 +1,91 @@
+-- Задание 2
+-- Название и продолжительность самого длительного трека.
+SELECT name, length FROM tracks ORDER BY length DESC LIMIT 1;
+
+
+-- Название треков, продолжительность которых не менее 3,5 минут.
+SELECT name, length FROM tracks WHERE length >= 3.5;
+
+
+-- Названия сборников, вышедших в период с 2018 по 2020 год включительно.
+SELECT name FROM collections WHERE year BETWEEN 2018 AND 2020;
+
+
+-- Исполнители, чьё имя состоит из одного слова.
+SELECT name FROM artists WHERE name NOT LIKE '% %';
+
+
+-- Название треков, которые содержат слово «мой» или «my».
+SELECT name FROM tracks WHERE name LIKE '%My%' OR name LIKE '%my%';
+
+
+
+-- Задание 3
+-- Количество исполнителей в каждом жанре.
+SELECT name, COUNT(artistid) q FROM genres g 
+JOIN artistsgenres a ON g.id = a.genreid
+GROUP BY g.name
+ORDER BY q DESC;
+
+
+-- Количество треков, вошедших в альбомы 2019–2020 годов.
+SELECT COUNT(t.id) FROM tracks t 
+JOIN albums a ON a.id = t.album
+WHERE a.year BETWEEN 2019 AND 2020;
+
+
+-- Средняя продолжительность треков по каждому альбому.
+SELECT a.name, AVG(length) FROM albums a 
+JOIN tracks t ON a.id = t.album
+GROUP BY a.name;
+
+
+-- Все исполнители, которые не выпустили альбомы в 2020 году.
+SELECT a.name FROM artists a 
+JOIN albumartists ar ON a.id = ar.artistid 
+JOIN albums al ON ar.albumid = al.id
+WHERE al.year != 2020;
+
+
+-- Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).
+SELECT DISTINCT c.name FROM collections c
+JOIN collectiontracks ct ON c.id = ct.collectionid 
+JOIN tracks t ON ct.trackid = t.id 
+JOIN albums a ON t.album = a.id 
+JOIN albumartists ar ON a.id = ar.artistid 
+WHERE ar.artistid = '2';
+
+
+
+-- Задание 4
+-- Названия альбомов, в которых присутствуют исполнители более чем одного жанра.
+SELECT a.name FROM albums a
+JOIN albumartists ar ON a.id = ar.albumid
+JOIN artists a2 ON ar.artistid = a2.id 
+JOIN artistsgenres a3 ON a2.id = a3.artistid
+GROUP BY a.id
+HAVING COUNT(a3.genreid) > 1;
+
+
+-- Наименования треков, которые не входят в сборники.
+SELECT t.name FROM tracks t 
+WHERE t.id NOT IN (SELECT c2.trackid FROM collectiontracks c2 JOIN tracks t2 ON c2.trackid = t2.id);
+
+
+-- Исполнитель или исполнители, написавшие самый короткий по продолжительности трек, — теоретически таких треков может быть несколько.
+SELECT a3.name FROM tracks t
+JOIN albums a ON t.album = a.id 
+JOIN albumartists a2 ON a.id = a2.albumid
+JOIN artists a3 ON a2.artistid = a3.id
+WHERE t.length = (SELECT MIN(length) FROM tracks)
+GROUP BY a3.name;
+
+
+-- Названия альбомов, содержащих наименьшее количество треков.
+SELECT name FROM (SELECT a.name name, COUNT(t.id) ct FROM albums a
+JOIN tracks t ON a.id = t.album
+GROUP BY a.name)
+WHERE ct = (SELECT MIN(ct) FROM (SELECT a.name name, COUNT(t.id) ct FROM albums a
+JOIN tracks t ON a.id = t.album
+GROUP BY a.name))
+GROUP BY name;
